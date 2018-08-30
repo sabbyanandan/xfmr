80279850c27f06a3eef2a287b121b872cf29a37e
Sabby Anandan <sabby.anandan@gmail.com>
2018-08-30 07:14:20 -0700

    Map to new host

--- ci/register-docker-image.sh
+++ ci/register-docker-image.sh
@@ -11,7 +11,7 @@ echo "Registering xfmr ${THE_VERSION}"
 # Replace `<HOST>` with the hostname of the SCDF-server running in Kubernetes.
 curl \
 	-X \
-	POST "http://35.233.133.167/apps/processor/xfmr?force=true" \
+	POST "http://35.230.5.225/apps/processor/xfmr?force=true" \
 	-uuser:password \
     -d "uri=docker:sabby/xfmr:${THE_VERSION}" \
 	-v
