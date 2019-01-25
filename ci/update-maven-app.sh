1078fd359042773e505767af48e593a15fdcdb6a
Sabby Anandan <sabby.anandan@gmail.com>
2019-01-25 14:45:44 -0800

    Temporarily comment CURLs

--- ci/update-maven-app.sh
+++ ci/update-maven-app.sh
@@ -9,9 +9,9 @@ THE_VERSION=$(grep -o '<revision[^"]*' pom.xml | sed -e 's/<revision>\(.*\)<\/re
 echo "Updating xfmr in CF with ${THE_VERSION} of Maven artifact"
 
 # Replace `<ROUTE>` with the route of SCDF-server running in CF.
-curl \
-	-X \
-	POST "http://sabby-test-dataflow-server.cfapps.io/streams/deployments/update/fooxfmr" \
-    -d '{"updateProperties":{"version.xfmr":"'"${THE_VERSION}"'"},"releaseName":"fooxfmr","packageIdentifier":{"packageName":"fooxfmr"}}' \
-    -H "Content-Type: application/json" \
-    -v
+# curl \
+# 	-X \
+# 	POST "http://sabby-test-dataflow-server.cfapps.io/streams/deployments/update/fooxfmr" \
+#     -d '{"updateProperties":{"version.xfmr":"'"${THE_VERSION}"'"},"releaseName":"fooxfmr","packageIdentifier":{"packageName":"fooxfmr"}}' \
+#     -H "Content-Type: application/json" \
+#     -v
