febe2d11b48a4d9bc4dcc9c0e23dfd1955381b72
Sabby Anandan <sabby.anandan@gmail.com>
2019-01-25 13:47:20 -0800

    Incremental commit for registering apps

---
+++ ci/latest/register-docker-image.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e -u
+
+cd xfmr
+
+THE_VERSION=$(grep -o '<revision[^"]*' pom.xml | sed -e 's/<revision>\(.*\)<\/revision>/\1/')
+
+echo "Registering xfmr ${THE_VERSION}"
+
+# Replace `<HOST>` with the hostname of the SCDF-server running in Kubernetes.
+# curl \
+# 	-X \
+# 	POST "http://35.230.5.225/apps/processor/xfmr?force=true" \
+# 	-uuser:password \
+#     -d "uri=docker:sabby/xfmr:${THE_VERSION}" \
+# 	-d "metadata-uri=maven://org.springframework.cloud.stream.app:cassandra-sink-rabbit:jar:metadata:1.3.1.RELEASE" \
+# 	-v
---
+++ ci/latest/register-maven-app.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e -u
+
+cd git-repo
+
+THE_VERSION=$(grep -o '<revision[^"]*' pom.xml | sed -e 's/<revision>\(.*\)<\/revision>/\1/')
+
+echo "Registering xfmr ${THE_VERSION}"
+
+# Replace `<ROUTE>` with the route of SCDF-server running in CF.
+# curl \
+# 	-X \
+# 	POST "http://sabby-test-dataflow-server.cfapps.io/apps/processor/xfmr?force=true" \
+#     -d "uri=maven://com.example:xfmr:${THE_VERSION}" \
+# 	-v
